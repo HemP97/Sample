de27d7d8eea64aa1747084e1c27b248c8a1c2390
HemP97 <hempatel1997@gmail.com>
2021-05-07 19:12:24 +0530

    Initial version
    Signed-off-by: HemP97 <hempatel1997@gmail.com>

--- run.sh
+++ run.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-iverilog -o simple.vvp simple.sv simple_tb.sv
-vvp simple.vvp
 if [ $1 = "lint" ]
 then
-    verilator --lint-only -Wall simple.sv
+    verilator --lint-only -Wall sample.sv
 elif [ $1 = "sim" ]
 then
+    iverilog -o sample.vvp sample.sv sample_tb.sv
+    vvp sample.vvp
     gtkwave simple.vcd
+elif [ $1 = "clean" ]
+then
+    rm -rf sample.vvp simple.vcd
 fi
---
+++ sample.sv
@@ -0,0 +1,9 @@
+module sample(A, B);
+
+   input  [3:0] A;
+   output [3:0] B;
+
+   // mix up the input bits
+   assign B = { A[0], A[2], A[1], A[3] };
+
+endmodule
